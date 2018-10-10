dd7f61fae704965629ce65f89dd56be5cdc50da9
Janos Szathmary <szathmary.janos89@gmail.com>
2018-10-10 20:34:34 +0200

    DEV-51 user resource added

---
+++ src/main/kotlin/com/strydal/backend/user/UserService.kt
@@ -0,0 +1,25 @@
+package com.strydal.backend.user
+
+import com.strydal.backend.base.BaseService
+import com.strydal.backend.base.ID
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+internal class UserService(private val userDao: UserDao) : BaseService<User, UserWithId> {
+
+    @Transactional(readOnly = false)
+    override fun insert(entity: User): ID = userDao.insert(entity).value
+
+    @Transactional(readOnly = false)
+    override fun update(entity: UserWithId) = userDao.update(entity)
+
+    @Transactional(readOnly = false)
+    override fun deleteById(id: ID) = userDao.deleteById(id)
+
+    override fun findAll(): List<UserWithId> = userDao.findAll()
+
+    override fun findById(id: ID): UserWithId? = userDao.findById(id)
+
+}
